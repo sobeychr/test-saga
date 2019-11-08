cf806deb8903956e614cffa2e710ee4afd6f3de6
Christopher Sobey <christopher.sobey@mindgeek.com>
2019-11-08 10:47:17 -0500

    - Adjusted CSS by gender

--- src/component/chatUser/index.jsx
+++ src/component/chatUser/index.jsx
@@ -7,11 +7,17 @@ import Header from './header';
 import Input from './input';
 import MessageBox from './messageBox';
 
+const isMale = user => user.gender === 0;
+
 const ChatUser = ({user}) => {
     const dispatch = useDispatch();
+    const classes = [
+        'chatuser',
+        isMale(user) ? 'chatuser_male' : 'chatuser_female'
+    ];
 
     return (
-        <div className='chatuser'>
+        <div className={classes.join(' ')}>
             <Header user={user}/>
             <MessageBox user={user}/>
             <Input dispatch={dispatch} user={user}/>
