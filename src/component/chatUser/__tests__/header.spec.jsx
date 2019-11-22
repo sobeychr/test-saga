7c6d4c8d7d917962fa17a09db4c965739117dc49
Christopher Sobey <christopher.sobey@mindgeek.com>
2019-11-22 15:38:11 -0500

    + Added mocks + Added UniTest ! incomplete mocks

--- src/component/chatUser/__tests__/header.spec.jsx
+++ src/component/chatUser/__tests__/header.spec.jsx
@@ -1,19 +1,19 @@
 import React from 'react';
 import { shallow } from 'enzyme';
-import { userMock } from 'Util/mock';
 
-import Avatar from './../avatar';
+import Header from './../avatar';
 
-describe('src/component/charUser/avatar', () => {
+describe('src/component/charUser/header', () => {
     const title = 'test-title';
+    const user = { id: 1, name: 'test1'};
 
     it('should render properly', () => {
-        const wrapper = shallow(<Avatar user={userMock} title={title}/>);
+        const wrapper = shallow(<Avatar user={user} title={title}/>);
         const classes = wrapper.prop('className');
 
         expect(wrapper).toHaveLength(1);
         expect(classes).toEqual(expect.stringContaining('avatar'));
-        expect(classes).toEqual(expect.stringContaining('4'));
+        expect(classes).toEqual(expect.stringContaining('1'));
         expect(wrapper.prop('title')).toEqual(title);
     });
 });
--- src/util/__tests__/user.spec.jsx
+++ src/util/__tests__/user.spec.jsx
@@ -1,28 +1,25 @@
 import { getEntry, getEntries } from './../user';
+import { userListMock } from './../mock';
 
 describe('src/util/user', () => {
-    const userList = [
-        { id: 1, name: 'test1' },
-        { id: 2, name: 'test2' },
-        { id: 3, name: 'test3' },
-        { id: 4, name: 'test4' },
-        { id: 5, name: 'test5' },
-    ];
-
     it('should handle "getEntry"', () => {
-        expect(getEntry(userList, 1)).toEqual({ id: 1, name: 'test1' });
-        expect(getEntry(userList, 3)).toEqual({ id: 3, name: 'test3' });
+        expect(getEntry(userList, 1)).toMatchObject(
+            expect.objectContaining({ id: 1 })
+        );
+        expect(getEntry(userList, 3)).toMatchObject(
+            expect.objectContaining({ id: 3 })
+        );
         expect(getEntry(userList, 7)).toBeUndefined();
     });
 
     it('should handle "getEntries"', () => {
         expect(getEntries(userList, [1, 3])).toEqual([
-            { id: 1, name: 'test1' },
-            { id: 3, name: 'test3' },
+            expect.objectContaining({ id: 1 }),
+            expect.objectContaining({ id: 3 }),
         ]);
         expect(getEntries(userList, [])).toEqual([]);
         expect(getEntries(userList, [7, 10, 1])).toEqual([
-            { id: 1, name: 'test1' },
+            expect.objectContaining({ id: 1 }),
         ]);
     });
 });
